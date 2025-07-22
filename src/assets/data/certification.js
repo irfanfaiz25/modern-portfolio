69cec3abc92e6aa95e08eb64d70d28efa47f121d
irfanfaiz25 <ir.faiz2000@gmail.com>
2025-07-22 12:55:23 +0700

    fix: modify cv download button

--- src/assets/data/certification.js
+++ src/assets/data/certification.js
@@ -6,12 +6,12 @@ export const CertificationData = [
     name: "Letter of Participation in Internship Program",
     publisher: "PT Astra Otoparts",
     year: "2022",
-    image: SQLCertificate,
+    image: AstraCertificate,
   },
   {
     name: "SQL Course Certificate",
     publisher: "Solo Learn",
     year: "2022",
-    image: AstraCertificate,
+    image: SQLCertificate,
   },
 ];
